fae89286c39e0ba539ff87bda1d449215b8ea5a0
= <=>
2021-10-13 21:04:24 +0300

    Добавьте файлы проекта.

---
+++ number_classificator/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace number_classificator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label0 = new System.Windows.Forms.Label();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(669, 390);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(110, 30);
+            this.label9.TabIndex = 27;
+            this.label9.Text = "9 ";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label9.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(669, 350);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(110, 30);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "8 ";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label8.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(669, 310);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 30);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "7 ";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label7.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(669, 270);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 30);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "6 ";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label6.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(669, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 30);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "5 ";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label5.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(669, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 30);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "4 ";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label4.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(669, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 30);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "3 ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(669, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 30);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "2 ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(669, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 30);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "1 ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Visible = false;
+            // 
+            // label0
+            // 
+            this.label0.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.label0.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label0.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label0.ForeColor = System.Drawing.Color.White;
+            this.label0.Location = new System.Drawing.Point(669, 30);
+            this.label0.Name = "label0";
+            this.label0.Size = new System.Drawing.Size(110, 30);
+            this.label0.TabIndex = 18;
+            this.label0.Text = "0 ";
+            this.label0.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label0.Visible = false;
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.BackColor = System.Drawing.Color.Azure;
+            this.ResultLabel.Font = new System.Drawing.Font("Palatino Linotype", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ResultLabel.ForeColor = System.Drawing.Color.Purple;
+            this.ResultLabel.Location = new System.Drawing.Point(417, 161);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(216, 125);
+            this.ResultLabel.TabIndex = 17;
+            this.ResultLabel.Text = "This number must be ";
+            this.ResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.ResultLabel.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MistyRose;
+            this.button2.Font = new System.Drawing.Font("Palatino Linotype", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.DarkRed;
+            this.button2.Location = new System.Drawing.Point(417, 310);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(216, 101);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Classify";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MistyRose;
+            this.button1.Font = new System.Drawing.Font("Palatino Linotype", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1.Location = new System.Drawing.Point(417, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 101);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(358, 372);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label0);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Number Classification";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label0;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ number_classificator/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Number_classificatorML.Model;
+
+namespace number_classificator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DrawingSuface = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            DrawGraph = Graphics.FromImage(DrawingSuface);
+            pictureBox1.BackgroundImage = DrawingSuface;
+            pictureBox1.BackgroundImageLayout = ImageLayout.None;
+        }
+
+        bool isPressed = false;
+        Bitmap DrawingSuface;
+        Graphics DrawGraph;
+        Point CurrentPoint;
+        Point PreviousPoint;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ActiveControl = label1;
+            ModelInput input = new ModelInput()
+            {
+                ImageSource = "image.png"
+            };
+
+            ModelOutput output = ConsumeModel.Predict(input);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isPressed = true;
+            CurrentPoint = e.Location;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isPressed = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isPressed)
+            {
+                PreviousPoint = CurrentPoint;
+                CurrentPoint = e.Location;
+                Paint();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height))
+            {
+                pictureBox1.DrawToBitmap(bmp, new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height));
+                bmp.Save("image.png", ImageFormat.Png);
+            }
+
+
+
+            //prediction(image.png);
+            ModelInput input = new ModelInput()
+            {
+                ImageSource = "image.png"
+            };
+
+            ModelOutput output = ConsumeModel.Predict(input);
+
+            label0.Text += GetScore(output.Score[0]) + "%";
+            label1.Text += GetScore(output.Score[1]) + "%";
+            label2.Text += GetScore(output.Score[2]) + "%";
+            label3.Text += GetScore(output.Score[3]) + "%";
+            label4.Text += GetScore(output.Score[4]) + "%";
+            label5.Text += GetScore(output.Score[5]) + "%";
+            label6.Text += GetScore(output.Score[6]) + "%";
+            label7.Text += GetScore(output.Score[7]) + "%";
+            label8.Text += GetScore(output.Score[8]) + "%";
+            label9.Text += GetScore(output.Score[9]) + "%";
+
+            //here we fill labels
+
+            ActiveControl = label0;
+            ResultLabel.Text += Environment.NewLine + output.Prediction;
+            ResultLabel.Visible = true;
+            ShowReslutls();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ActiveControl = label0;
+            DrawGraph.Clear(Color.White);
+            RefreshScreen();
+        }
+
+        void Paint()
+        {
+            Pen p = new Pen(Color.Black, 1);
+            DrawGraph.FillRectangle(Brushes.Black, new Rectangle(new Point(CurrentPoint.X - 7, CurrentPoint.Y - 7), new Size(14, 14)));
+            DrawGraph.DrawRectangle(p, new Rectangle(new Point(CurrentPoint.X - 7, CurrentPoint.Y - 7), new Size(14, 14)));
+            pictureBox1.Invalidate();
+        }
+
+        void RefreshScreen()
+        {
+            pictureBox1.Refresh();
+            ResultLabel.Text = "This number must be";
+            ResultLabel.Visible = false;
+            label0.Text = "0 ";
+            label1.Text = "1 ";
+            label2.Text = "2 ";
+            label3.Text = "3 ";
+            label4.Text = "4 ";
+            label5.Text = "5 ";
+            label6.Text = "6 ";
+            label7.Text = "7 ";
+            label8.Text = "8 ";
+            label9.Text = "9 ";
+            label0.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+            label9.Visible = false;
+        }
+
+        void ShowReslutls()
+        {
+            label0.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            label7.Visible = true;
+            label8.Visible = true;
+            label9.Visible = true;
+        }
+
+        string GetScore(float score)
+        {
+            if (score < 0.001) return "< 0.1";
+            return "= " + Math.Round((100 * score), 2).ToString();
+        }
+    }
+}
